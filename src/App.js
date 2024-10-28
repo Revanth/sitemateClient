b97a2e577d8d373db7a7ff97dd951c3a09ae4342
Revanth <Revanth@users.noreply.github.com>
2024-10-28 13:10:04 +1100

    Created an individual function for each button

--- src/App.js
+++ src/App.js
@@ -2,25 +2,65 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const jiraIssue = {'ID': 'Bug001', 'Title': 'Sample Title', 'Description': 'Sample description'}
 
-  const apiCall = async(apiName) => {
-    const jiraIssue = {'ID': 'Bug001', 'Title': 'Sample Title', 'Description': 'Sample description'}
+  const apiCallCreate = async() => {
     try{
-      const res = await fetch('http://localhost:3001/sitemate/api/'+apiName)
+      const res = await fetch('http://localhost:3001/sitemate/api/create', {
+        method: 'POST',
+        headers:{'content-Type': 'application/json'},
+        body: JSON.stringify(jiraIssue)
+      })
       console.log('Response:', res)
     }
     catch(e){
-      console.log('Api '+ apiName +' error: ' + e)
+      console.log('Create Api ' +' error: ' + e)
+    }
+  }
+
+  const apiCallRead = async() => {
+    try{
+      const res = await fetch('http://localhost:3001/sitemate/api/read')
+      console.log('Response:', res)
+    }
+    catch(e){
+      console.log('Read Api ' +' error: ' + e)
+    }
+  }
+
+  const apiCallUpdate = async() => {
+    try{
+      const res = await fetch('http://localhost:3001/sitemate/api/update', {
+        method: 'PUT',
+        headers:{'content-Type': 'application/json'},
+        body: JSON.stringify(jiraIssue)
+      })
+      console.log('Response:', res)
+    }
+    catch(e){
+      console.log('Update Api ' +' error: ' + e)
+    }
+  }
+
+  const apiCallDelete = async(id) => {
+    try{
+      const res = await fetch(`http://localhost:3001/sitemate/api/delete/${id}`, {
+        method: 'DELETE'
+      })
+      console.log('Response:', res)
+    }
+    catch(e){
+      console.log('Delete Api ' +' error: ' + e)
     }
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={() => apiCall('create')}>Create</button>
-        <button onClick={() => apiCall('read')}>Read</button>
-        <button onClick={() => apiCall('update')}>Update</button>
-        <button onClick={() => apiCall('delete')}>Delete</button>
+        <button onClick={apiCallCreate}>Create</button>
+        <button onClick={apiCallRead}>Read</button>
+        <button onClick={apiCallUpdate}>Update</button>
+        <button onClick={() => apiCallDelete('Bug001')}>Delete</button>
       </header>
     </div>
   );
